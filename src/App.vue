c392c4264269b22167e6d5c1be6bdca586676e39
Favour <toluwaniokunowo@gmail.com>
2023-09-16 20:30:24 -0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <div class="min-h-full">
+    <div class="bg-gray-800 pb-32">
+      <Navbar></Navbar>
+
+      <header class="py-10">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
+        </div>
+      </header>
+    </div>
+
+    <main class="-mt-32">
+      <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+          <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+        </div>
+        <!-- /End replace -->
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,93 @@
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Music Videos", href: "#", current: false },
+  { name: "Films", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
+</script>
+
+<template>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div class="border-b border-gray-700">
+        <div class="flex h-16 items-center px-4 sm:px-0">
+          <div class="flex items-center w-full justify-between">
+            <div class="flex-shrink-0">
+              <img
+                class="h-8 w-8"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
+                  :class="[
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'px-3 py-2 rounded-md text-sm font-medium',
+                  ]"
+                  :aria-current="item.current ? 'page' : undefined"
+                  >{{ item.name }}</a
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton
+              class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="border-b border-gray-700 md:hidden">
+      <div class="space-y-1 px-2 py-3 sm:px-3">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block px-3 py-2 rounded-md text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
